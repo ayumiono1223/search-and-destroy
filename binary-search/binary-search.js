c9cc88df852380495e8ecb3424416cf7414340f8
Ayumi Ono <ayumiono.1223@gmail.com>
2019-01-28 15:03:52 -0500

    done with Bynary-search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,27 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+
+  let n = 0;
+  let lastOfRawIdx = Math.pow(2, n) - 1;
+
+  while (target > array[lastOfRawIdx]) {
+    if (lastOfRawIdx > array.length - 1) {
+      lastOfRawIdx = array.length - 1;
+      break;
+    } else {
+      n++;
+      lastOfRawIdx += Math.pow(2, n);
+    }
+  }
+  let beginingOfRowIdx = lastOfRawIdx - Math.pow(2, n) + 1;
+  for (let i = beginingOfRowIdx; i <= lastOfRawIdx; i++) {
+    if (array[i] === target) {
+      console.log(array[i])
+      return true;
+    }
+  }
+  return false;
 };
 
 /*
